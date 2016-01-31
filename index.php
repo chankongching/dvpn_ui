0dc3ec22a12ced1046fa3f2c790276693f5452e5
Nginx web server <nginx@ip-10-0-0-130.ap-northeast-1.compute.internal>
2016-01-31 11:55:09 +0000

    Changing file structure

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+require_once '/tools/functions.php';
+$instanceID = explode(" ",shell_exec('tools/ec2-metadata -i'))[1];
+$elasticIP = explode(" ",shell_exec('tools/ec2-metadata -v'))[1];
+echo "Instance ID = " . $instanceID; 
+echo "Curren IP = " . $elasticIP;
+
 // Require the Composer autoloader.
 require 'vendor/autoload.php';
 
