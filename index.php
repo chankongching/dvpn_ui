40447c4964e90a64a4da4bf557fb0cf06e0fdc51
Jacky Chan <chankongching@gmail.com>
2016-02-03 02:06:16 +0800

    Update plans of dvpn. Commit changes of new frontend

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
-<?php
+<html>
+	<head>
+		<title>Dvpn_ui</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+
+
+
+<body class="is-loading">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<section id="main">
+						<header>
+					
+							<h1>Dvpn UI</h1>
+							<p>UI version K1</p>
+						</header>
+                        
+                        <?php
 require_once 'tools/functions.php';
 
 // Prepare variables.
@@ -61,3 +88,26 @@ $line = fgets($README);
 fclose($f);
 echo 'BootDev dvpn version v' . explode("=",$line)[1];
 ?>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="copyright">
+							<li>&copy; Jacky Chan</li>
+							<li>Design: Jacky Chan</li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<!--[if lte IE 8]><script src="assets/js/respond.min.js"></script><![endif]-->
+			<script>
+				if ('addEventListener' in window) {
+					window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-loading\b/, ''); });
+					document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+				}
+			</script>
+
+	</body>
+</html>
