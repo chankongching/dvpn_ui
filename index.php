6ffb0b01c13d7064493aa8b83daff3205ee8327e
Nginx web server <nginx@ip-10-0-0-202.ap-southeast-1.compute.internal>
2016-02-05 11:31:48 +0000

    Finishing frontend modification

--- index.php
+++ index.php
@@ -29,7 +29,6 @@
 
 <?php
 require_once 'tools/functions.php';
-
 // Prepare variables.
 // Run ec2-metadata to get instances information
 $instanceID = explode(" ",shell_exec('tools/ec2-metadata -i'))[1];
@@ -57,16 +56,6 @@ if ($handle) {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Show ICON
-// Check if action is set
-if(!empty($_REQUEST['action'])){
-    $action_set = true;
-}
-
-if(isset($action_set) && $action_set){
-    if($_REQUEST['action'] == 'refresh'){
-        include 'refresh.php';
-    }
-}
 ?>
 <form class="form-no-horizontal-spacing" id="refreshForm" action="index.php?action=refresh" method="post">
     <button class="btn btn-primary btn-cons" type="submit" ><p>IP </p><img src="images/refresh.png" alt="Change IP" style="max-width:100%;max-height:100%;height:80%;vertical-align:middle;position: relative;top: -3px;" /></button>
@@ -80,33 +69,41 @@ if(isset($action_set) && $action_set){
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Show INFO
 //echo "Instance ID = " . $instanceID . '<br>';
+// Check if action is set
+if(!empty($_REQUEST['action'])){
+    $action_set = true;
+}
+if(isset($action_set) && $action_set && $_REQUEST['action'] == 'refresh'){
+    include 'refresh.php';
+}
+
+if(isset($action_set) && $action_set && $_REQUEST['action'] == 'refresh'){
 ?>
-                         <b>Current IP: </b>
-                         <!--
-                         <button id="target-to-copy" data-clipboard-target="clipboard-text">
-                         -->
-<!--
-                         <input type="text" id="copyTarget" value="<?php echo $elasticIP; ?>">
-                         <button id="copyButton">Copy</button>
--->
-<!--
-                         <p name="clipboard-text" id="clipboard-text" class="clipboard-text">
-<?php 
-    echo $elasticIP; 
+<h4><b>Expired IP: </b></h4>
+<?php
+} else {
 ?>
-                         </p>
--->
-                         <!--<img src="images/copy.png" alt="Click to copy" style="max-width:100%;max-height:100%;height:80%;vertical-align:middle;position: relative;top: -3px;" />-->
-                         <!--</button>-->
-<!--
-<textarea id="txt"><?php echo $elasticIP; ?></textarea>
-<div align="center"><button class="btn-md" onclick="copy();">copy</button></div>
--->
-<button id='markup-copy'>Copy Button</button>
-<textarea id="txt"><?php echo $elasticIP; ?></textarea>
-<p><i>Copy only works in Desktop Browser</i><p>
+<h4><b>Current IP: </b></h4>
+<?php
+}
+?>
+<button id='markup-copy'><img src="images/copy.png" alt="Click to copy" style="max-width:100%;max-height:100%;height:80%;vertical-align:middle;position: relative;top: -3px;" />
+</button>
+<textarea id="markup-copy"><?php echo $elasticIP; ?></textarea>
+
+<?php
+if(!empty($new_ip)){
+?>
+<h4><b>New IP: </b></h4>
+<button id='markup-copy-new'><img src="images/copy.png" alt="Click to copy" style="max-width:100%;max-height:100%;height:80%;vertical-align:middle;position: relative;top: -3px;" /></button>
+<textarea id="markup-copy"><?php echo $new_ip; ?></textarea>
+<?php
+}
+
+?>
+<p id="message"><i>Copy only works on Desktop</i><p>
 <script>
-    var my_var = "<?php echo $elasticIP; ?>";
+    var curr_ip = "<?php echo $elasticIP; ?>";
 function addMultipleListeners(element,events,handler,useCapture,args){
   if (!(events instanceof Array)){
     throw 'addMultipleListeners: '+
@@ -122,11 +119,22 @@ function addMultipleListeners(element,events,handler,useCapture,args){
   }
 }
 
-function handler(e) {
+function handler_curr(e) {
   // do things
         clipboard.copy({
-            'text/plain': my_var,
-            'text/html': my_var 
+            'text/plain': curr_ip,
+            'text/html': curr_ip 
+        }).then(
+            function(){console.log('success'); },
+            function(err){console.log('failure', err);
+        });
+};
+var new_ip = "<?php if(!empty($new_ip))echo $new_ip;?>";
+function handler_new(e) {
+  // do things
+        clipboard.copy({
+            'text/plain': new_ip,
+            'text/html': new_ip
         }).then(
             function(){console.log('success'); },
             function(err){console.log('failure', err);
@@ -135,7 +143,9 @@ function handler(e) {
 
 // usage
 addMultipleListeners(document.getElementById('markup-copy'),
-                     ['touchstart','click'],handler,false);
+                     ['touchstart','click'],handler_curr,false);
+addMultipleListeners(document.getElementById('markup-copy-new'),
+                     ['touchstart','click'],handler_new,false);
 </script>
 <?php
 //echo "VPC = " . $vpc_id . '<br>';
@@ -182,6 +192,7 @@ echo 'console version v' . explode("=",$line)[1];
             } );
         </script>
         -->
+<!--
     <script>
         function copy()
         {
@@ -196,5 +207,6 @@ echo 'console version v' . explode("=",$line)[1];
             }
         }
     </script>
+-->
     </body>
 </html>
--- refresh.php
+++ refresh.php
@@ -60,5 +60,4 @@ if($get_ip){
         //'PublicIp' => $elasticIP,
     ]);
 }
-echo '<br>';
-echo 'New IP = ' . $allocation["PublicIp"];
+$new_ip = $allocation["PublicIp"];
